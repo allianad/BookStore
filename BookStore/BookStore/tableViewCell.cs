f0ef95d5a0fde87216c10c59b578eb7f9f2a2d39
allianad <32075424+allianad@users.noreply.github.com>
2017-10-19 14:01:22 -0600

    updated

---
+++ BookStore/BookStore/tableViewCell.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BookStore
+{
+    public partial class tableViewCell : UIView
+    {
+        public tableViewCell (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
